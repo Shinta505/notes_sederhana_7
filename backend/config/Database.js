ca74626f911f93671933194d39429bd0e2571238
Shinta505 <shinta.nur505@gmail.com>
2025-03-09 00:36:47 +0700

    Initial Commit

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const dbContext = new Sequelize('notes_shinta', 'root', '', {
-    host: 'localhost',
+    host: '34.132.1.143',
     dialect: 'mysql'
 });
 
--- backend/models/NoteModel.js
+++ backend/models/NoteModel.js
@@ -2,16 +2,23 @@ import { DataTypes, Sequelize } from "sequelize";
 import dbContext from "../config/Database.js";
 
 const Note = dbContext.define('notes', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     isi: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    label: { 
+    label: {
         type: DataTypes.STRING,
         allowNull: false
     }
 }, {
-    freezeTableName: true
+    tableName: 'notes',
+    freezeTableName: true,
+    timestamps: true
 });
 
 export { Note };
