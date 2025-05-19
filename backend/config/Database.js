6a5e4a6ea50bd64b4c2c7624dd7e13ec58946870
Shinta Nursobah Chairani <shinta.nur505@gmail.com>
2025-05-19 13:40:26 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 const DB_NAME = notes_shinta;
 const DB_USERNAME = root;
 const DB_PASSWORD = 
-const DB_HOST = 34.132.1.143;
+const DB_HOST = "34.132.1.143";
 
 const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
